75de9ebda56eee8e2e01d91a13a465a28a72ead8
Devop-code <kamgadevop@gmail.com>
2025-02-10 13:46:09 +0100

    Ajout de la logique pour récupérer et supprimer des notes sur la page d'accueil, avec gestion des erreurs.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import { useState, useEffect } from "react";
+import api from "../api";
 const HomePage = () => {
-  return (
-    <div><h1>the home pages for authenticated user</h1></div>
-  )
-}
+  const [note, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  useEffect(()=>{
+     getNote();
+  },[])
+  const getNote = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => {setNotes(data); console.log(data)})
+      .catch((error) => alert(error));
+  };
+  const deletNotes = (id)=>{
+    api.delete(`/api/notes/delete/${id}/`).then((res)=>{
+     if(res.status === 200){
+      alert("Note deleted!")
+     }
+     else{
+      alert("Error deleting note")
+     }
+    }).catch((error)=> alert())
+  }
+  return <div>home</div>;
+};
 
 export default HomePage
